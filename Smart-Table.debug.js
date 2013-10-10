0b52079a04212157efb01218e5f5342e23b1106a
Beyers Cronje <bcronje@gmail.com>
2013-10-11 00:51:42 +0200

    Update pagination template to bootstrap 3

--- Smart-Table.debug.js
+++ Smart-Table.debug.js
@@ -646,12 +646,9 @@ angular.module("partials/globalSearchCell.html", []).run(["$templateCache", func
 
 angular.module("partials/pagination.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("partials/pagination.html",
-    "<div class=\"pagination\">\n" +
-    "    <ul>\n" +
-    "        <li ng-repeat=\"page in pages\" ng-class=\"{active: page.active, disabled: page.disabled}\"><a\n" +
-    "                ng-click=\"selectPage(page.number)\">{{page.text}}</a></li>\n" +
-    "    </ul>\n" +
-    "</div> ");
+    "<ul class=\"pagination\">\n" +
+    "  <li ng-repeat=\"page in pages\" ng-class=\"{active: page.active, disabled: page.disabled}\"><a ng-click=\"selectPage(page.number)\">{{page.text}}</a></li>\n" +
+    "</ul>");
 }]);
 
 angular.module("partials/selectAllCheckbox.html", []).run(["$templateCache", function($templateCache) {
